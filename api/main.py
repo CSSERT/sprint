12d128dcb96af7fea5d715b305b5d9baa35a094c
Nguyễn Phúc Toàn <ngphuc_toan@tutanota.com>
2026-02-22 07:43:11 +0700

    Backup

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from v1.routers import health
+import v1.routers as v1
 
 app = FastAPI(
     title="Sprint API",
     version="1.0.0",
 )
 
-app.include_router(health.router, prefix="/v1")
+app.include_router(v1.router)
